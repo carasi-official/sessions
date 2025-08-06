e3fba770a0a6afbe2790cb1fa8070dc56f80478c
Carasi <carasi-official@gmail.com>
2025-08-06 15:59:50 -0700

    working with async await to get the display countdown to wait 1 second before changing

--- sessions/src/routes/timer.js
+++ sessions/src/routes/timer.js
@@ -2,84 +2,92 @@ import { useState } from "react";
 
 function Timer(){
     //grab dom elements
-    const timerButton = document.getElementById('timerButton');
-    const pause = document.getElementById('pauseTime');
-    const resume = document.getElementById('resumeTime');
-    const display = document.getElementById('display');
 
-    //set useState variables
-    //const [minutes, setMinutes] = useState(3);
-    //const [seconds, setSeconds] = useState(59);
-    let minutes = 3;
+    let pause = document.getElementById('pauseTime');
+    let resume = document.getElementById('resumeTime');
+
+
+    const seconds = 59;
+    let counterMinutes = 2; //eventually this will prompt the user for a value and subtract the result by one
+    let counterSeconds = 0;
     let timer;
-    for(let x = 0; x < 3; x++){
-        console.log(minutes);
-        minutes--;
+
+    //the async needs to happen in the second function, not the minutes
+
+    async function counter(){
+        return new Promise((resolve)=>{
+            let display = document.getElementById('display');
+            let currentDisplayTime = document.getElementById('displayTime')
+            let p = document.createElement('p');
+            currentDisplayTime.remove();
+            console.log(`${counterMinutes}:${counterSeconds}`);
+            if (counterMinutes < 10){
+                if (counterSeconds < 10){
+                    p.innerHTML = `0${counterMinutes}:0${counterSeconds}`;
+                }else{
+                    p.innerHTML =`0${counterMinutes}:${counterSeconds}`;
+                }
+            }else{
+                if (counterSeconds < 10){
+                    p.innerHTML = `${counterMinutes}:0${counterSeconds}`;
+                }else{
+                    p.innerHTML= `${counterMinutes}:${counterSeconds}`;
+                }
+            }
+            p.id = 'displayTime';
+            display.append(p);
+            resolve("seconds function called");
+        }, 1000);
+    }
+
+    async function secondsFunc(){
+
+        let counterSecs = counterSeconds; //unfortunate reliance on a global variable
+        for (let x = 0; x <= counterSecs; x++){
+            const result = await counter();
+            counterSeconds--;
+        }
+        counterSeconds = seconds; // global variable reset
 
     }
-    clearInterval(timer);
+
+    function counterFunc () {
+        let counterMins = counterMinutes;
+        for(let x = counterMins; x > 0; x--){
+            secondsFunc();
+            counterMinutes--;
+        }
+    }
+
     //two counters - one for minutes, one for seconds
     //two loops - one for the 60 seconds, one for the minutes. when min and seconds = 0 -- countdown complete
 
-    // function countDown(){
-    //     timer = setInterval(()=>{
-    //         let currentDisplayTime = document.getElementById('displayTime')
-    //         let p = document.createElement('p');
-    //         currentDisplayTime.remove();
-    //
-    //         if(seconds >= 0){
-    //             if (seconds < 10){
-    //                 p.innerHTML = `${minutes} : 0${seconds}`;
-    //             }else{
-    //                 p.innerHTML = `${minutes} : ${seconds}`;
-    //             }
-    //             seconds--;
-    //             p.id = 'displayTime';
-    //             display.append(p);
-    //         }else{
-    //             if(minutes === 0){
-    //                 p.innerHTML = `Count Down Complete`;
-    //                 p.id = 'displayTime';
-    //                 display.append(p);
-    //             }else{
-    //                     minutes--;
-    //                     seconds = 59;
-    //             }
-    //         }
-    //     }, 1000)
-    // }
 
-    timerButton.addEventListener('click', ()=>{
-        timerButton.style.display = 'none';
-        console.log(minutes);
-        seconds = 59;
-        if (minutes > -1 && seconds > -1){
-            minutes--;
-            countDown();
-        }
-    })
+    let timerButtonClick = () =>{
+        counterFunc();
+    }
 
-    pause.addEventListener("click", ()=>{
+    let pauseClick = () =>{
         console.log('pause clicked');
         clearInterval(timer);
-        console.log(minutes, seconds);
-    })
+    }
 
-    resume.addEventListener("click", ()=>{
+    let resumeClick = () =>{
         console.log('resume clicked');
-        countDown();
-    })
-
+        counterFunc();
+    }
 
 
     return (
         <div>
-            <button id="timerButton">Start Count Down</button>
+            <button id="timerButton" onClick={timerButtonClick}>Start Count Down</button>
             <div id="display">
-                <p id="displayTime">{minutes} : 00</p>
+                {/* test - this is an odd way to do comments but sure*/}
+                {/*load display with a function?*/}
+                <p id="displayTime">{counterMinutes+1}:00</p>
             </div>
-            <button id="pauseTime">Pause</button>
-            <button id="resumeTime">Resume</button>
+            <button id="pauseTime" onClick={pauseClick}>Pause</button>
+            <button id="resumeTime" onClick={resumeClick}>Resume</button>
             <button id="reset">Reset</button>
         </div>
     )
